06f57460570b12ed245e3fdb1ee14adf406b6e8b
Mattias.Poppe <poppe.mattias@gmail.com>
2025-08-08 15:12:45 +0200

    C02P20-9 Run cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,12 @@
 extern crate serde;
 use futures::TryStreamExt;
 use pulsar::{
-    message::proto::command_subscribe::SubType, message::Payload, Consumer,
-    DeserializeMessage, Pulsar, TokioExecutor,
+    message::proto::command_subscribe::SubType, message::Payload, Consumer, DeserializeMessage,
+    Pulsar, TokioExecutor,
 };
-use tokio_postgres::NoTls;
 use pulsar2db::*;
 use std::path::Path;
+use tokio_postgres::NoTls;
 
 #[derive(Serialize, Deserialize)]
 struct TestData {
@@ -35,7 +35,7 @@ const TOPICS: [&'static str; 7] = [
 /// Used to derive the "base-pid" from pids for collaterals, eg.:
 /// `<pid>_srt`. Returns the original input string if it doesn't
 /// contain an underscore.
-fn split_pid_by_underscore(pid: &str) -> &str{
+fn split_pid_by_underscore(pid: &str) -> &str {
     let result: Vec<&str> = pid.split('_').collect();
     result[0]
 }
@@ -47,7 +47,6 @@ fn filename_from_path(full_path: Option<&str>) -> Option<&str> {
     filename.to_str()
 }
 
-
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
     env_logger::init();
@@ -55,11 +54,16 @@ async fn main() -> Result<(), anyhow::Error> {
     // Get our configuration from the environment
     // The necessary environment variables can be found in the `.env` file
     let config = match envy::from_env::<Config>() {
-       Ok(config) => config,
-       Err(error) => panic!("{:#?}", error)
+        Ok(config) => config,
+        Err(error) => panic!("{:#?}", error),
     };
 
-    log::info!("Connecting to Pulsar on {}: topics={:?}, subscription_name={}", &config.pulsar_host, &TOPICS, &config.pulsar_subscription_name);
+    log::info!(
+        "Connecting to Pulsar on {}: topics={:?}, subscription_name={}",
+        &config.pulsar_host,
+        &TOPICS,
+        &config.pulsar_subscription_name
+    );
     let addr = format_pulsar_connection_string(&config);
     let pulsar: Pulsar<_> = Pulsar::builder(addr, TokioExecutor).build().await?;
 
@@ -85,7 +89,7 @@ async fn main() -> Result<(), anyhow::Error> {
             eprintln!("connection error: {}", e);
         }
     });
-    
+
     let mut counter = 0usize;
     while let Some(msg) = consumer.try_next().await? {
         consumer.ack(&msg).await?;
@@ -100,14 +104,19 @@ async fn main() -> Result<(), anyhow::Error> {
         counter += 1;
         log::trace!("got {} messages", counter);
         log::debug!("{:?}", &data);
-        log::info!("insert into DB: {}, correlation_id: {}", &data.type_field.as_str(), &data.correlation_id.as_str());
+        log::info!(
+            "insert into DB: {}, correlation_id: {}",
+            &data.type_field.as_str(),
+            &data.correlation_id.as_str()
+        );
         match data.type_field.as_str() {
             // Legacy sip create event: sip created on FTP
             "be.meemoo.sipin.sip.create" => {
                 let status: &str = "SIP_CREATED";
                 let filename = filename_from_path(data.data["path"].as_str());
-                let res = client.execute(
-                    "INSERT INTO sipin_sips (
+                let res = client
+                    .execute(
+                        "INSERT INTO sipin_sips (
                         correlation_id,
                         bag_name,
                         cp_id,
@@ -124,41 +133,46 @@ async fn main() -> Result<(), anyhow::Error> {
                         last_event_date,
                         status)
                     VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12,
-                            $13, $14, $15)", &[
-                        &data.correlation_id.as_str(),
-                        &filename.unwrap(),
-                        &data.data["cp_id"].as_str(),
-                        &data.data["local_id"].as_str(),
-                        &data.data["md5_hash_essence_manifest"].as_str(),
-                        &data.data["md5_hash_essence_sidecar"].as_str(),
-                        &data.data["essence_filename"].as_str(),
-                        &data.data["essence_filesize"].as_i64(),
-                        &data.data["host"].as_str(),
-                        &data.data["path"].as_str(),
-                        &data.data["bag_filesize"].as_i64(),
-                        &data.time,
-                        &data.type_field.as_str(),
-                        &data.time,
-                        &status,
-                    ],
-                ).await;
+                            $13, $14, $15)",
+                        &[
+                            &data.correlation_id.as_str(),
+                            &filename.unwrap(),
+                            &data.data["cp_id"].as_str(),
+                            &data.data["local_id"].as_str(),
+                            &data.data["md5_hash_essence_manifest"].as_str(),
+                            &data.data["md5_hash_essence_sidecar"].as_str(),
+                            &data.data["essence_filename"].as_str(),
+                            &data.data["essence_filesize"].as_i64(),
+                            &data.data["host"].as_str(),
+                            &data.data["path"].as_str(),
+                            &data.data["bag_filesize"].as_i64(),
+                            &data.time,
+                            &data.type_field.as_str(),
+                            &data.time,
+                            &status,
+                        ],
+                    )
+                    .await;
                 match res {
                     Ok(rows) => log::debug!("Rows created: {}", rows),
                     Err(error) => log::error!("Problem: {:?}", error),
                 };
-            },
+            }
             // Legacy and new bag transfer events
             "be.meemoo.sipin.bag.transfer" => {
                 let status: &str = "BAG_TRANSFERRED_TO_SIPIN";
-                let res = client.execute(
-                    "UPDATE sipin_sips SET last_event_type=$1, last_event_date=$2, status=$3
-                    WHERE correlation_id=$4", &[
-                        &data.type_field.as_str(),
-                        &data.time,
-                        &status,
-                        &data.correlation_id.as_str(),
-                    ],
-                ).await;
+                let res = client
+                    .execute(
+                        "UPDATE sipin_sips SET last_event_type=$1, last_event_date=$2, status=$3
+                    WHERE correlation_id=$4",
+                        &[
+                            &data.type_field.as_str(),
+                            &data.time,
+                            &status,
+                            &data.correlation_id.as_str(),
+                        ],
+                    )
+                    .await;
                 match res {
                     Ok(rows) => match rows {
                         0 => log::warn!("No rows updated for event {}! correlation_id {} not present?", &data.type_field.as_str(), &data.correlation_id.as_str()),
@@ -167,19 +181,22 @@ async fn main() -> Result<(), anyhow::Error> {
                     },
                     Err(error) => log::error!("Problem: {:?}", error),
                 };
-            },
+            }
             // Legacy and new bag unzip events
             "be.meemoo.sipin.bag.unzip" => {
                 let status: &str = "BAG_UNZIPPED";
-                let res = client.execute(
-                    "UPDATE sipin_sips SET last_event_type=$1, last_event_date=$2, status=$3
-                    WHERE correlation_id=$4", &[
-                        &data.type_field.as_str(),
-                        &data.time,
-                        &status,
-                        &data.correlation_id.as_str(),
-                    ],
-                ).await;
+                let res = client
+                    .execute(
+                        "UPDATE sipin_sips SET last_event_type=$1, last_event_date=$2, status=$3
+                    WHERE correlation_id=$4",
+                        &[
+                            &data.type_field.as_str(),
+                            &data.time,
+                            &status,
+                            &data.correlation_id.as_str(),
+                        ],
+                    )
+                    .await;
                 match res {
                     Ok(rows) => match rows {
                         0 => log::warn!("No rows updated for event {}! correlation_id {} not present?", &data.type_field.as_str(), &data.correlation_id.as_str()),
@@ -188,19 +205,22 @@ async fn main() -> Result<(), anyhow::Error> {
                     },
                     Err(error) => log::error!("Problem: {:?}", error),
                 };
-            },
+            }
             // Legacy and new bag validate events
             "be.meemoo.sipin.bag.validate" => {
                 let status: &str = "BAG_VALIDATED";
-                let res = client.execute(
-                    "UPDATE sipin_sips SET last_event_type=$1, last_event_date=$2, status=$3
-                    WHERE correlation_id=$4", &[
-                        &data.type_field.as_str(),
-                        &data.time,
-                        &status,
-                        &data.correlation_id.as_str(),
-                    ],
-                ).await;
+                let res = client
+                    .execute(
+                        "UPDATE sipin_sips SET last_event_type=$1, last_event_date=$2, status=$3
+                    WHERE correlation_id=$4",
+                        &[
+                            &data.type_field.as_str(),
+                            &data.time,
+                            &status,
+                            &data.correlation_id.as_str(),
+                        ],
+                    )
+                    .await;
                 match res {
                     Ok(rows) => match rows {
                         0 => log::warn!("No rows updated for event {}! correlation_id {} not present?", &data.type_field.as_str(), &data.correlation_id.as_str()),
@@ -209,19 +229,22 @@ async fn main() -> Result<(), anyhow::Error> {
                     },
                     Err(error) => log::error!("Problem: {:?}", error),
                 };
-            },
+            }
             // Legacy sip validate event
             "be.meemoo.sipin.sip.validate" => {
                 let status: &str = "SIP_VALIDATED";
-                let res = client.execute(
-                    "UPDATE sipin_sips SET last_event_type=$1, last_event_date=$2, status=$3
-                    WHERE correlation_id=$4", &[
-                        &data.type_field.as_str(),
-                        &data.time,
-                        &status,
-                        &data.correlation_id.as_str(),
-                    ],
-                ).await;
+                let res = client
+                    .execute(
+                        "UPDATE sipin_sips SET last_event_type=$1, last_event_date=$2, status=$3
+                    WHERE correlation_id=$4",
+                        &[
+                            &data.type_field.as_str(),
+                            &data.time,
+                            &status,
+                            &data.correlation_id.as_str(),
+                        ],
+                    )
+                    .await;
                 match res {
                     Ok(rows) => match rows {
                         0 => log::warn!("No rows updated for event {}! correlation_id {} not present?", &data.type_field.as_str(), &data.correlation_id.as_str()),
@@ -230,7 +253,7 @@ async fn main() -> Result<(), anyhow::Error> {
                     },
                     Err(error) => log::error!("Problem: {:?}", error),
                 };
-            },
+            }
             // Legacy aip (mh-sip) create event
             "be.meemoo.sipin.aip.create" => {
                 let status: &str = "AIP_CREATED";
@@ -254,18 +277,21 @@ async fn main() -> Result<(), anyhow::Error> {
                     },
                     Err(error) => log::error!("Problem: {:?}", error),
                 };
-            },
+            }
             "be.meemoo.sipin.aip.transfer" => {
                 let status: &str = "AIP_DELIVERED_TO_MAM";
-                let res = client.execute(
-                    "UPDATE sipin_sips SET last_event_type=$1, last_event_date=$2, status=$3
-                    WHERE correlation_id=$4", &[
-                        &data.type_field.as_str(),
-                        &data.time,
-                        &status,
-                        &data.correlation_id.as_str(),
-                    ],
-                ).await;
+                let res = client
+                    .execute(
+                        "UPDATE sipin_sips SET last_event_type=$1, last_event_date=$2, status=$3
+                    WHERE correlation_id=$4",
+                        &[
+                            &data.type_field.as_str(),
+                            &data.time,
+                            &status,
+                            &data.correlation_id.as_str(),
+                        ],
+                    )
+                    .await;
                 match res {
                     Ok(rows) => match rows {
                         0 => log::warn!("No rows updated for event {}! correlation_id {} not present?", &data.type_field.as_str(), &data.correlation_id.as_str()),
@@ -274,10 +300,10 @@ async fn main() -> Result<(), anyhow::Error> {
                     },
                     Err(error) => log::error!("Problem: {:?}", error),
                 };
-            },
+            }
             _ => {
                 log::warn!("Unknown event type: {:#?}", &data.type_field.as_str());
-            },
+            }
         }
     }
 
